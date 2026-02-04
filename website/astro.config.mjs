c69a764f85111a88f87c474c29486b442d7861a7
Ignasi Barrera <ignasi@tetrate.io>
2026-02-04 12:24:02 +0100

    review comments
    Signed-off-by: Ignasi Barrera <ignasi@tetrate.io>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import mermaid from "astro-mermaid";
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
+	integrations: [
+		mdx(),
+		mermaid({
+			theme: "neutral",
+			autoTheme: true,
+		}),
+	],
 });
