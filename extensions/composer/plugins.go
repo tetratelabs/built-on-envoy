77f4b75dc73258aa39cc9cf5c0f9f7bed4fbad26
wbpcode <wbphub@gmail.com>
2026-02-12 21:11:22 +0800

    fix lint
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- extensions/composer/plugins.go
+++ extensions/composer/plugins.go
@@ -1,14 +1,16 @@
+// Copyright Built On Envoy
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+// Package composer provides built-in plugins for the composer binary.
 package composer
 
 import (
-	// Register built-in plugins into the binary. Because only one golang shared library
-	// can be loaded into a process, we need to register all built-in plugins here and
-	// build them into the same binary.
-
-	// Go plugin to loader other composer plugins that be compiled into separate shared libraries.
-	_ "github.com/tetratelabs/built-on-envoy/extensions/composer/goplugin"
 	// Example built-in plugin.
 	_ "github.com/tetratelabs/built-on-envoy/extensions/composer/example/embedded"
+	// Go plugin to loader other composer plugins that be compiled into separate shared libraries.
+	_ "github.com/tetratelabs/built-on-envoy/extensions/composer/goplugin"
 	// WAF plugin.
 	_ "github.com/tetratelabs/built-on-envoy/extensions/composer/waf/embedded"
 )
