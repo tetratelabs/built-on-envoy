4a2a46164e3d4bacd7c5640e43ae902c13bdb8e6
Marc Cirauqui <marc@tetrate.io>
2026-02-27 12:21:31 +0100

    checks
    Signed-off-by: Marc Cirauqui <marc@tetrate.io>

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -4,7 +4,6 @@ export const PROJECT_NAME = 'Built On Envoy';
 export const CLI_NAME = 'boe';
 
 export const GITHUB_REPO = 'https://github.com/tetratelabs/built-on-envoy';
-export const GITHUB_REPO_RAW = 'tetratelabs/built-on-envoy';
 export const EXTENSIONS_PATH = 'extensions';
 
 export const WEBSITE = 'https://builtonenvoy.io';
