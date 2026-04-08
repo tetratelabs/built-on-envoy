0b12b42be828cf08df3a978deeb57a701d970482
wbpcode <wbphub@gmail.com>
2026-04-08 11:20:52 +0800

    fix format lint
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- extensions/composer/pkg/utility.go
+++ extensions/composer/pkg/utility.go
@@ -1,7 +1,14 @@
+// Copyright Built On Envoy
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
 package pkg
 
 import "github.com/envoyproxy/envoy/source/extensions/dynamic_modules/sdk/go/shared"
 
+// GetMostSpecificConfig is a helper function to get the most specific config of type T from
+// the filter handle.
 func GetMostSpecificConfig[T any](handle shared.HttpFilterHandle) T {
 	var zero T
 	mostSpecificConfig := handle.GetMostSpecificConfig()
