d1c1aae8a3d06d322f6f397e8e2f2baeacd5b1bc
Alessandro Vermeulen <github@spockz.nl>
2026-08-12 11:31:19 +0200

    Added copyrights Signed-off-by: Alessandro Vermeulen <github@spockz.nl>
    Signed-off-by: Alessandro Vermeulen <github@spockz.nl>

--- extensions/composer/dynamic-fault-injection/run-local.sh
+++ extensions/composer/dynamic-fault-injection/run-local.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+# Copyright Built On Envoy
+# SPDX-License-Identifier: Apache-2.0
+# The full text of the Apache license is available in the LICENSE file at
+# the root of the repo.
+
 #
 
 CONFIG=$(cat <<-END
